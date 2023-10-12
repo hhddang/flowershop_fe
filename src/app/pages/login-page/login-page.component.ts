541c25f43d7a223038f32593f2a5550aa9e3f1e7
hhddang <dangtramthien@gmail.com>
2023-10-12 16:41:21 +0700

    Add: login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+  email: String = '';
+  password: String = '';
 
+  checkEmail(): boolean {
+    if (this.email.includes('@') && this.email.includes('.')) return true;
+    return false;
+  }
+
+  login() {
+    if (this.checkEmail()) {
+      // login
+    }
+  }
 }
